32956f458e0f1edd86f4f52e4305bc717edc2710
lgramirez <gramirez.developer@gmail.com>
2022-01-14 23:51:22 -0400

    Adding Entity Framework Core and Migrations

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // a traves de este constructor puedo pasarle distintas configuraciones a EF
+        // como ser el connection string que apunta a la DB que vamos a usar
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // le a EF que cree una tabla llamada Autores con el modelo Autor
+        public DbSet<Autor> Autores { get; set; }
+
+        // para crear una migracion
+        // dotnet ef migrations add Inicial
+        // Inicial -> es el nombre de la migracion
+
+        // para correr la migracion
+        // dotnet ef database update
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace WebApiAutores
 {
     public class Startup
@@ -12,6 +14,10 @@ namespace WebApiAutores
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
