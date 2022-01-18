f600f28443d9ca113439f7a33c1c1edcd9cec82b
lgramirez <gramirez.developer@gmail.com>
2022-01-18 11:42:29 -0400

    Add default validations to model

--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +10,14 @@ namespace WebApiAutores.Entidades
     public class Autor
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} no debe tener mas de {1} caracteres.")]
         public string Nombre { get; set; }
+
+        [Range(18, 120)]
+        // el no mapped nos permite decirle que no necesitamos este atributo en la tabla Autor
+        [NotMapped]
+        public int Edad { get; set; }
 
         // propiedad de navegacion
         public List<Libro> Libros { get; set; }
