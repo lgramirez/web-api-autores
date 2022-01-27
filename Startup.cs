f27cb2da068990f6b5c1f39e72322858fdf09a6c
lgramirez <gramirez.developer@gmail.com>
2022-01-27 13:05:29 -0400

    Add custom middleware to log http responses

--- Startup.cs
+++ Startup.cs
@@ -25,8 +25,43 @@ namespace WebApiAutores
             services.AddSwaggerGen();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+
+            app.Use(async (contexto, siguiente) =>
+            {
+                using (var ms = new MemoryStream())
+                {
+                    var cuerpoOriginalRespuesta = contexto.Response.Body;
+                    contexto.Response.Body = ms;
+
+                    // con este metodo pasamos al siguiente middleware
+                    await siguiente.Invoke();
+
+                    // despues del invoke se hara todo lo siguiente cuando los middlewares esten volviendo a este middleware
+                    // para finalizar la cadena de procesos
+
+                    ms.Seek(0, SeekOrigin.Begin);
+                    string respuesta = new StreamReader(ms).ReadToEnd();
+                    ms.Seek(0, SeekOrigin.Begin);
+
+                    await ms.CopyToAsync(cuerpoOriginalRespuesta);
+                    contexto.Response.Body = cuerpoOriginalRespuesta;
+
+                    logger.LogInformation(respuesta);
+                }
+            });
+
+            // este metodo es para decir que solo en esta ruta se ejecuta este middleware
+            // app.Map("/ruta1", app =>
+            // {
+            //     // este metodo hace que sea el unico middleware que se ejecuta e intercepta todo
+            //     app.Run(async contexto =>
+            //     {
+            //         await contexto.Response.WriteAsync("Estoy interceptando la tuberia.");
+            //     });
+            // });
+
             if (env.IsDevelopment())
             {
                 app.UseSwagger();
